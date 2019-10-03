b825be677d371d25776eda6d14876d06058c2e5c
Gautam <baghel.g@husky.neu.edu>
2019-10-03 17:15:11 -0400

    test #6

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const shell = require('shelljs')
-
 shell.exec('./entrypoint.sh')
-
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
