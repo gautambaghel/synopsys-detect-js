3a710883295ae500714331759851d102311c11b5
Gautam <baghel.g@husky.neu.edu>
2019-10-03 17:13:09 -0400

    test #5

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,8 @@
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-      echo "linux based os detected running bash script..."
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-      echo "mac detected running bash script..."
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-      echo "linux emulated windows detected running debian bash script..."
-elif [[ "$OSTYPE" == "msys" ]]; then
-      echo "lightweight windows detected running powershell bash script..."
-elif [[ "$OSTYPE" == "win32" ]]; then
-     echo "windows detected running powershell script..."
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-     echo "freebsd detected running debian bash script..."
-else
-     echo "Unknown OS detected trying debian bash script..."
-fi
-
 #!/bin/bash
+
+if ! type "$curl" > /dev/null; then
+  echo install curl before proceeding 
+  exit 1
+fi
 
 bash <(curl -s https://detect.synopsys.com/detect.sh) "$*"
