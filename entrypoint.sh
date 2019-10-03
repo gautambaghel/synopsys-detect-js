79bf38e981ae68a12904919d564eec57c1ffcb5d
Gautam <baghel.g@husky.neu.edu>
2019-10-02 22:04:49 -0400

    test #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-export JAVA_HOME="/usr/local/openjdk-8"
-
-set -e
-
-if [[ -z "${BLACKDUCK_URL}" || -z "${POLARIS_URL}" ]]; then
-  echo "Set atleast one of Black Duck or Polaris URL secret variable."
-  exit 1
-fi
-
 bash <(curl -s https://detect.synopsys.com/detect.sh) "$*"
