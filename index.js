3aede9469379934f2f40fd3dffb03df41f7ebc2c
Gautam <>
2019-10-03 17:40:20 -0400

    test #7

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const shell = require('shelljs')
-shell.exec('./entrypoint.sh')
+const http = require('http');
+const fs = require('fs');
+
+const file = fs.createWriteStream("detect.sh");
+const request = http.get("http://detect.synopsys.com/detect.sh", function(response) {
+  response.pipe(file);
+});
+
+fs.chmodSync('detect.sh', '777');
+shell.exec('./detect.sh')
