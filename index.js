0809667fb640efcc2d82bc649de992beb20860fe
Gautam <>
2019-10-03 18:41:40 -0400

    test 9

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const shell = require('shelljs')
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const spawn = require('child_process').spawn;
 
-var file_url = "https://detect.synopsys.com/detect.sh"
-var file_name = url.parse(file_url).pathname.split('/').pop();
-var file = fs.createWriteStream(file_name);
-var curl = spawn('curl', [file_url]);
-curl.stdout.on('data', function(data) { file.write(data); });
-curl.stdout.on('end', function(data) {
-  fs.chmodSync('detect.sh', '777');
-  file.end();
-  fs.closeSync(fd)
-  console.log(file_name + ' script downloaded!');
-  shell.exec('./detect.sh');
-});
-curl.on('exit', function(code) {
-  if (code != 0) {
-    console.log('Failed to download detect: ' + code);
-  }
-});
+shell.exec("wget https://detect.synopsys.com/detect.sh")
+shell.exec("chmod +x detect.sh")
+shell.exec("./detect.sh")
