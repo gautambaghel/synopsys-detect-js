4f94e7451faf39948a79ff3f603ebee8921fec82
Gautam <>
2019-10-04 12:09:46 -0400

    test #28

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const IS_WINDOWS = process.platform === 'win32';
 const detectArgs = core.getInput('args');
 if (IS_WINDOWS) {
     // On windows use the POWERSHELL SCRIPT
-    shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`)
+    shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`)
   } else {
     // On everything else do bash
     shell.exec("wget https://detect.synopsys.com/detect.sh")
