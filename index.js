d829d63b4eda6914023c36f3636c9892425ab257
Gautam <>
2019-10-04 18:33:07 -0400

    return code test #2

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const IS_WINDOWS = process.platform === 'win32';
 const detectArgs = core.getInput('args');
 if (IS_WINDOWS) {
     // On windows use the POWERSHELL SCRIPT
-    shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`)
+    var returnCode = shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`).code
   } else {
     // On everything else do bash
     shell.exec("wget https://detect.synopsys.com/detect.sh")
     shell.exec("chmod +x detect.sh")
-    var sandwich = shell.exec(`./detect.sh ${detectArgs}`).code
-    console.log(sandwich)
+    var returnCode = shell.exec(`./detect.sh ${detectArgs}`).code
+
+    core.warning("SEE HOW THIS LOOKS LIKE")
 }
