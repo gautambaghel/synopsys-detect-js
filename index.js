e6568e9ee72f38f3e56eb5d1f1076baeb2265faf
Gautam <>
2019-10-03 20:23:01 -0400

    test #20

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const shell = require('shelljs')
+const IS_WINDOWS = process.platform === 'win32';
 
-shell.exec("wget https://detect.synopsys.com/detect.sh")
-shell.exec("chmod +x detect.sh")
-shell.exec("./detect.sh")
+const detectArgs = core.getInput('detectArgs');
+if (IS_WINDOWS) {
+    // On windows use the POWERSHELL SCRIPT
+    shell.exec("wget https://detect.synopsys.com/detect.ps1")
+    shell.exec("powershell \"[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm detect.ps1?$(Get-Random) | iex; detect\"")
+  } else {
+    // On everything else do bash
+    shell.exec("wget https://detect.synopsys.com/detect.sh")
+    shell.exec("chmod +x detect.sh")
+    shell.exec("./detect.sh ${detectArgs}")
+}
