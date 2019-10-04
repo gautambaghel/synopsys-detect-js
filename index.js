5368c32c31c8b23658bebef10600781b55bebdc7
Gautam <>
2019-10-03 20:32:34 -0400

    test #23

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ if (IS_WINDOWS) {
     // On everything else do bash
     shell.exec("wget https://detect.synopsys.com/detect.sh")
     shell.exec("chmod +x detect.sh")
-    shell.exec("./detect.sh ${detectArgs}")
+    console.log(`The event payload: ${detectArgs}`)
+    shell.exec("./detect.sh ${{detectArgs}}")
 }
