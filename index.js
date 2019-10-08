23c4c32baacd53afe4c303236f394680b0bc79ba
Gautam <>
2019-10-08 12:53:00 -0400

    test #6

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const shell = require('shelljs')
 const IS_WINDOWS = process.platform === 'win32';
 
 const detectArgs = core.getInput('args');
+var returnCode = 0
 if (IS_WINDOWS) {
     // On windows use the POWERSHELL SCRIPT
-    var returnCode = shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`).code
+    returnCode = shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`).code
   } else {
     // On everything else do bash
     shell.exec("wget https://detect.synopsys.com/detect.sh")
     shell.exec("chmod +x detect.sh")
-    var returnCode = shell.exec(`./detect.sh ${detectArgs}`).code
+    returnCode = shell.exec(`./detect.sh ${detectArgs}`).code
+}
 
-    process.exit(127)
+if (returnCode != 0 || returnCode != 3) {
+    process.exit(returnCode)
 }
