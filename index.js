fc310b82d77ef349ac3f61514ec62f9b8bc9c046
Gautam <>
2019-10-14 17:11:48 -0400

    testing core exec #1

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const shell = require('shelljs')
-const IS_WINDOWS = process.platform === 'win32';
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+const IS_WINDOWS = process.platform === 'win32'
 
-const detectArgs = core.getInput('args');
+let myOutput = ''
+let myError = ''
+
+const options = {}
+options.listeners = {
+  stdout: (data: Buffer) => {
+    myOutput += data.toString()
+  },
+  stderr: (data: Buffer) => {
+    myError += data.toString()
+  }
+}
+options.cwd = './lib'
+
+const detectArgs = core.getInput('args')
 var returnCode = 0
 if (IS_WINDOWS) {
     // On windows use the POWERSHELL SCRIPT
-    returnCode = shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`).code
+    await exec.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`, options)
+    // returnCode = shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`).code
   } else {
     // On everything else do bash
-    shell.exec("wget https://detect.synopsys.com/detect.sh")
-    shell.exec("chmod +x detect.sh")
-    returnCode = shell.exec(`./detect.sh ${detectArgs}`).code
+    await exec.exec("wget https://detect.synopsys.com/detect.sh && chmod +x detect.sh")
+    await exec.exec(`./detect.sh ${detectArgs}`, options)
+    //shell.exec("wget https://detect.synopsys.com/detect.sh")
+    //shell.exec("chmod +x detect.sh")
+    // returnCode = shell.exec(`./detect.sh ${detectArgs}`).code
 }
 
 if (returnCode == 3) {
@@ -25,3 +42,6 @@ if (returnCode == 3) {
 if (returnCode != 0) {
     core.setFailed(`Synopsys Detect failed with error ${returnCode}`)
 }
+
+core.warning(myOutput)
+core.warning(myError)
